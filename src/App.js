f2aaafcf6a55da0b03387fe048c760cf4f2fd3e5
Agustín Mafud <agustin.mafud@globant.com>
2020-08-31 22:25:56 -0300

    Material UI added and files cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+// @ packages
 import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+// @ own
 import './App.css';
 
-function App() {
+const App = () => {
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: 'Quicksand, sans-serif',
+      fontSize: 16,
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>Initial template</div>
+    </ThemeProvider>
   );
 }
 
