b6d9b014ad772bbc23824144ad3cdb6e2a061e3a
Agustín Mafud <agustin.mafud@globant.com>
2020-09-25 00:58:31 -0300

    buid created

---
+++ build/precache-manifest.b8edcf1f7feed0203afb0b5cba6552ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16f48648f978ad859b140e45795e66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "146308bba4ef6766cfa2",
+    "url": "/static/css/main.b3e79bdb.chunk.css"
+  },
+  {
+    "revision": "fdc8bed257dc2be0384f",
+    "url": "/static/js/2.a3f75dbc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a3f75dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146308bba4ef6766cfa2",
+    "url": "/static/js/main.01643dad.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
