2b48ac16db9b4b6e8a29d87f95ceba59bd90a956
Agustín Mafud <agustin.mafud@globant.com>
2020-09-26 12:45:42 -0300

    build for github pages created

---
+++ docs/precache-manifest.7e410f68b58a8f1645bf5afd0ef7d8b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f383a2d2348487ae9ef9f3a29ef784",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "39f16d6e01323117812f",
+    "url": "/todoapp/static/css/main.bff40ca7.chunk.css"
+  },
+  {
+    "revision": "00988ae1ea0404c9ab9e",
+    "url": "/todoapp/static/js/2.4c7f75e9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/todoapp/static/js/2.4c7f75e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f16d6e01323117812f",
+    "url": "/todoapp/static/js/main.8bba81e7.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9c45a250476f8f6dfa0f2a94579be49.js"
+  "/todoapp/precache-manifest.7e410f68b58a8f1645bf5afd0ef7d8b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
